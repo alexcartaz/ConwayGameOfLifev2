4e2c7a98eeb64c27300926243b65afe58abe6657
Alex <alexander.t.carter@gmail.com>
2019-07-19 14:04:57 -0700

    copying old files over

---
+++ build/precache-manifest.5d2a8a1c06db35e965ab280ce6b59355.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96b047903c91612f40bb1dd592abec5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2608d5a7ea397f30c288",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "a07987ce4114374c24de",
+    "url": "/static/js/2.e1860d32.chunk.js"
+  },
+  {
+    "revision": "2608d5a7ea397f30c288",
+    "url": "/static/js/main.a5515e7e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
